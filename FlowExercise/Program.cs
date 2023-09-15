b249602e33dc98ae8d1290dd7e9b3ab29ee77a69
DESKTOP-NS8HAV3 <may_aj@outlook.com>
2023-09-15 09:30:19 +0200

    commented code, fixed some bugs

--- FlowExercise/Program.cs
+++ FlowExercise/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System.Text.RegularExpressions;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace FlowExercise
 {
@@ -13,14 +12,14 @@ namespace FlowExercise
             Free = 0
         }
 
+        const int minLengthSentence = 3;
+
         static void Main(string[] args)
         {
             bool keepShowingCommands = true;
             while (keepShowingCommands)
             {
-                Console.WriteLine("Welcome. Please enter a command.");
                 ShowOptions();
-
                 string command = Console.ReadLine();
 
                 switch (command)
@@ -47,8 +46,12 @@ namespace FlowExercise
             }
         }
 
+        /// <summary>
+        /// Displays the options to the user.
+        /// </summary>
         public static void ShowOptions()
         {
+            Console.WriteLine("Please enter a command.");
             Console.WriteLine("[1] Adolescent or pensioner");
             Console.WriteLine("[2] Price for whole company");
             Console.WriteLine("[3] Repeat 10 times");
@@ -56,6 +59,9 @@ namespace FlowExercise
             Console.WriteLine("[0] Quit");
         }
 
+        /// <summary>
+        /// Gets the price for the person given an age.
+        /// </summary>
         private static int AdolescentOrOPensioner(int index = -1, bool isCompany = false)
         {
             string displayMsg;
@@ -74,6 +80,9 @@ namespace FlowExercise
 
         }
 
+        /// <summary>
+        /// Gets the price for a full company, given their ages.
+        /// </summary>
         private static void GetPriceForFullCompany()
         {
             string displayMsg = "How many persons are going to the cinema?";
@@ -88,20 +97,27 @@ namespace FlowExercise
             Print($"\nNr of people: {nrOfPeople}\nTotal price: {sum} SEK\n");
         }
 
+        /// <summary>
+        /// Takes a text as input and then prints it ten times on the same line.
+        /// </summary>
         private static void RepeatTenTimes()
         {
             Print("Enter an arbitrary text");
             string text = GetInput();
-            while (ValidateText(text))
+            while (!ValidateText(text))
             {
                 text = InvalidInputForText();
             }
             for(int i = 0; i < 10; i++) 
             {
-                Print(text);
+                Print2($"{i + 1}. {text} ");
             }
+            Print("");
         }
 
+        /// <summary>
+        /// Takes a user input sentence and displays the third word of that sentence.
+        /// </summary>
         private static void TheThirdWord()
         {
             string displayMsg = "Enter a sentence:";
@@ -119,27 +135,76 @@ namespace FlowExercise
 
         }
 
+        /// <summary>
+        /// Finds the correct price for the corresponding age.
+        /// </summary>
+        /// <param name="age">The age of the person.</param>
+        /// <param name="isCompany">If there is more than one person.</param>
+        /// <returns>The price for this age.</returns>
+        private static int CheckAgeAndGetPrice(int age, bool isCompany)
+        {
+
+            int ageTypePrice;
+            if (age < 5 || age > 100)
+            {
+                ageTypePrice = (int)AgeGroup.Free;
+            }
+            else if (age < 20)
+            {
+                ageTypePrice = (int)AgeGroup.Adolescent;
+            }
+            else if (age > 64)
+            {
+                ageTypePrice = (int)AgeGroup.Pensioner;
+            }
+            else
+            {
+                ageTypePrice = (int)AgeGroup.Regular;
+            }
+
+            if (!isCompany && ageTypePrice != 0)
+            {
+                Print($"{(AgeGroup) ageTypePrice} price: {ageTypePrice} SEK.\n");
+            }
+            else if (!isCompany && ageTypePrice == 0)
+            {
+                Print($"Children under 5 and centenarians: Free entrance.\n");
+            }
+
+            return ageTypePrice;
+        }
+
+        /// <summary>
+        /// Removes all whitespaces in a string and keeps the ones between each word inside the string.
+        /// </summary>
+        /// <param name="s">The string to format.</param>
+        /// <returns>The formatted string.</returns>
         private static string FormatString(string s)
         {
             s = Regex.Replace(s, @"\s+", " ");
             return s.Trim();
         }
 
+        /// <summary>
+        /// Checks if the length of the user input sentence is long enough.
+        /// </summary>
+        /// <param name="userSentence">The sentence to be checked.</param>
+        /// <returns>true if it's long enough, otherwise false.</returns>
         private static bool CheckLength(string userSentence)
         {
             string[] words = userSentence.Split(' ');
-            return SentenceIsLongEnough(words.Length);
-        }
-
-        private static bool SentenceIsLongEnough(int length)
-        {
-            if (length < 3)
+            if (words.Length < minLengthSentence)
             {
                 return false;
             }
             return true;
         }
 
+        /// <summary>
+        /// Displays the command for the user, validates the string input and returns it.
+        /// </summary>
+        /// <param name="message">The command for the use.r</param>
+        /// <returns>The validated input.</returns>
         private static string GetUserInput(string message)
         {
             Print(message);
@@ -151,6 +216,11 @@ namespace FlowExercise
             return input;
         }
 
+        /// <summary>
+        /// Displays the command for the user, validates the inputs that are expected to be integers and returns it.
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
         private static int ShowCommandAndGetUserInput(string message)
         {
             Print(message);
@@ -165,40 +235,11 @@ namespace FlowExercise
 
         }
 
-        private static int CheckAgeAndGetPrice(int age, bool isCompany)
-        {
-
-            int ageTypePrice;
-            if(age < 5 || age > 100) 
-            {
-                ageTypePrice = (int) AgeGroup.Free;
-            }
-            else if (age < 20)
-            {
-                ageTypePrice = (int) AgeGroup.Adolescent;
-            }
-            else if (age > 64) 
-            {
-                ageTypePrice = (int) AgeGroup.Pensioner;
-            } 
-            else
-            {
-                ageTypePrice = (int) AgeGroup.Regular;
-            }
-
-            var a = (AgeGroup)ageTypePrice;
-            if (!isCompany && ageTypePrice != 0)
-            {
-                Print($"{a} price: {ageTypePrice} SEK.\n");
-            } else if(!isCompany && ageTypePrice == 0)
-            {
-                Print($"Children under 5 and centenarians: Free entrance.\n");
-            }
-
-            return ageTypePrice;
-
-        }
-
+        /// <summary>
+        /// Validator that checks if the string contained only numbers, and if that number is in a range.
+        /// </summary>
+        /// <param name="num">the string number to be validated.</param>
+        /// <returns>the parsed number from string to int.</returns>
         private static int ValidateNumber(string? num)
         {
             if(!int.TryParse(num, out int parsedNum)) 
@@ -216,11 +257,20 @@ namespace FlowExercise
             return parsedNum;
         }
 
+        /// <summary>
+        /// Validator to check if a string is null or empty.
+        /// </summary>
+        /// <param name="text">the string text to be checked.</param>
+        /// <returns>true if not null or empty, otherwise false.</returns>
         private static bool ValidateText(string text)
         {
             return !string.IsNullOrEmpty(text);
         }
 
+        /// <summary>
+        /// If the input failed the validation, it displays it to the user and asks for input again.
+        /// </summary>
+        /// <returns>the new input by user.</returns>
         private static int InvalidInputForNumber()
         {
             Print("Try again:");
@@ -228,6 +278,10 @@ namespace FlowExercise
             return ValidateNumber(input);
         }
 
+        /// <summary>
+        /// If the input failed the validation, it displays it to the user and asks for input again.
+        /// </summary>
+        /// <returns>the new input by user.</returns>
         private static string InvalidInputForText()
         {
             Print("Can't be empty.");
@@ -236,11 +290,28 @@ namespace FlowExercise
             return sentence;
         }
 
+        /// <summary>
+        /// Prints to the console on new line.
+        /// </summary>
+        /// <param name="message">The message to be printed.</param>
         private static void Print(string message)
         {
             Console.WriteLine(message);
         }
 
+        /// <summary>
+        /// Prints to the console on same line.
+        /// </summary>
+        /// <param name="message">The message to be printed.</param>
+        private static void Print2(string message)
+        {
+            Console.Write(message);
+        }
+
+        /// <summary>
+        /// Reads from the console.
+        /// </summary>
+        /// <returns>the input from the console.</returns>
         private static string GetInput()
         {
             return Console.ReadLine();
